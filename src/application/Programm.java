8d27c4302e0890b5e7cfc1534976ff1812d93a8d
Higor Araujo <higoraraujobezerra@gmail.com>
2020-04-28 15:04:52 -0300

    Class: Position

--- src/application/Programm.java
+++ src/application/Programm.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Programm {
 
 	public static void main(String[] args) {
 		
-
+		Position pos = new Position(3,5);
+		
+		System.out.println(pos);
 	}
 
 }
